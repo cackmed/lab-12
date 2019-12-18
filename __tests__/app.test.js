081875ed4413cf00915682909d28572ee9b617ee
Caleb Pendergraft <cackmed@gmail.com>
2019-12-18 13:51:27 -0800

    wrote out rough tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Trade = require('../lib/models/trade');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -14,7 +15,161 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let trade;
+  beforeEach(async() => {
+    trade = await Trade.create({
+      year: new Date(),
+      Time_ref: new Date(),
+      magnitude: 3,
+      unit: 'Dollers',
+      country_code: 'UPC',
+      country_name: 'United Provinces of the County',
+      goods_exports: 1200,
+      goods_inports: 600,
+      services_exports: 300,
+      services_imports: 400,
+      total_exports: 1500,
+      total_imports: 1000,
+      two_way_trade: 2500
+    });
+  });
+
   afterAll(() => {
     return mongoose.connection.close();
+  });
+  it('can create a new trade entry', () => {
+    return request(app)
+      .post('/api/v1/trades')
+      .send({
+        byear: new Date(),
+        Time_ref: new Date(),
+        magnitude: 3,
+        unit: 'Dollers',
+        country_code: 'UPC',
+        country_name: 'United Provinces of the County',
+        goods_exports: 1200,
+        goods_inports: 600,
+        services_exports: 300,
+        services_imports: 400,
+        total_exports: 1500,
+        total_imports: 1000,
+        two_way_trade: 2500
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          year: expect.any(Date),
+          Time_ref: expect.any(Date),
+          magnitude: 3,
+          unit: 'Dollers',
+          country_code: 'UPC',
+          country_name: 'United Provinces of the County',
+          goods_exports: 1200,
+          goods_inports: 600,
+          services_exports: 300,
+          services_imports: 400,
+          total_exports: 1500,
+          total_imports: 1000,
+          two_way_trade: 2500,
+          __v: 0
+        });
+      });
+  });
+
+  it('gets a trade data entry by Id', () => {
+    return request(app)
+      .get(`/api/v1/trades/${trade.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          year: expect.any(Date),
+          Time_ref: expect.any(Date),
+          magnitude: 3,
+          unit: 'Dollers',
+          country_code: 'UPC',
+          country_name: 'United Provinces of the County',
+          goods_exports: 1200,
+          goods_inports: 600,
+          services_exports: 300,
+          services_imports: 400,
+          total_exports: 1500,
+          total_imports: 1000,
+          two_way_trade: 2500,
+          __v: 0
+        });
+      });
+  });
+
+  it('gets all trade data', () => {
+    return request(app)
+      .get('/api/v1/trades')
+      .then(res => {
+        expect(res.body).toEqual([
+          {
+            _id: expect.any(String),
+            year: expect.any(Date),
+            Time_ref: expect.any(Date),
+            magnitude: 3,
+            unit: 'Dollers',
+            country_code: 'UPC',
+            country_name: 'United Provinces of the County',
+            goods_exports: 1200,
+            goods_inports: 600,
+            services_exports: 300,
+            services_imports: 400,
+            total_exports: 1500,
+            total_imports: 1000,
+            two_way_trade: 2500,
+            __v: 0
+          }
+        ]);
+      });
+  });
+  it('updates a trade data entry', () => {
+    return request(app)
+      .patch(`/api/v1/trades/${trade.id}`)
+      .send({ goods_exports: 1500 })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          year: expect.any(Date),
+          Time_ref: expect.any(Date),
+          magnitude: 3,
+          unit: 'Dollers',
+          country_code: 'UPC',
+          country_name: 'United Provinces of the County',
+          goods_exports: 1500,
+          goods_inports: 600,
+          services_exports: 300,
+          services_imports: 400,
+          total_exports: 1500,
+          total_imports: 1000,
+          two_way_trade: 2500,
+          __v: 0
+        });
+      });
+  });
+  it('deletes a trade entry', () => {
+    return request(app)
+      .delete(`/api/v1/trades/${trade.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          year: expect.any(Date),
+          Time_ref: expect.any(Date),
+          magnitude: 3,
+          unit: 'Dollers',
+          country_code: 'UPC',
+          country_name: 'United Provinces of the County',
+          goods_exports: 1200,
+          goods_inports: 600,
+          services_exports: 300,
+          services_imports: 400,
+          total_exports: 1500,
+          total_imports: 1000,
+          two_way_trade: 2500,
+          __v: 0
+        });
+      });
   });
 });
